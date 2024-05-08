c6ca28098d857025e83b16c229bc8e7343a83494
Juan Carlos <juan.lara80@unach.mx>
2024-05-08 06:45:44 -0600

    Juan

--- apps/monorepo-nestjs/src/app.controller.ts
+++ apps/monorepo-nestjs/src/app.controller.ts
@@ -1,39 +1,58 @@
-import { Controller, Get, Inject, Post, Patch, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Inject, Post, Patch, Delete, Body, Render } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { UptadeUserDto, UserDto } from './dto/user.dto';
 import { IdDto } from './dto/id.dto';
 
 @Controller()
 export class AppController {
-  getHello(): any {
-    throw new Error('Method not implemented.');
-  }
   constructor(@Inject('MAIL_SERVICE') private userCrud: ClientProxy) {}
 
   @Post()
   async getUser(@Body() idDto: IdDto) {
     const { id } = idDto;
-    var result = this.userCrud.send('obtener', id);
+    const result = await this.userCrud.send('obtener', id).toPromise();
     return result;
   }
 
   @Post('crear')
-  async createUser(@Body() UserDto: UserDto) {
-
-    var result = this.userCrud.send('crear', UserDto);
+  async createUser(@Body() userDto: UserDto) {
+    const result = await this.userCrud.send('crear', userDto).toPromise();
     return result;
   }
 
   @Patch('actualizar')
   async updateUser(@Body() updateUserDto: UptadeUserDto) {
-    const result = await this.userCrud.send('actualizar', updateUserDto);
+    const result = await this.userCrud.send('actualizar', updateUserDto).toPromise();
     return result;
   }
 
   @Delete('eliminar')
   async deleteUser(@Body() idDto: IdDto) {
     const { id } = idDto;
-    var result = this.userCrud.send('eliminar', id);
+    const result = await this.userCrud.send('eliminar', id).toPromise();
     return result;
   }
+
+  @Get('usuarios')
+  async getUsers() {
+    const result = await this.userCrud.send('usuarios', {}).toPromise();
+    return result;
+  }
+
+  @Get('/home')
+  @Render('index') 
+  async hello() {
+    const users = await this.getUsers();
+    return { 
+      message: 'Sistema de Registros y Validaciones', 
+      users: users 
+    };
+  }
+
+  @Get('/login')
+  @Render('usuarios') 
+  async login() {
+    return { message: 'Por favor, inicia sesión' };
+  }
+
 }
